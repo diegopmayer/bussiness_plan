bcd7b2e11b7fabff62ceab3e1468d1f7e6aa95e9
diegopmayer <dpmayer@hotmail.com>
2019-02-28 23:48:45 -0300

    Fields created in forms

--- canvas/form.py
+++ canvas/form.py
@@ -3,3 +3,10 @@ from django import forms
 class CanvasForm(forms.Form):
     parceiro = forms.CharField()
     atividade = forms.CharField()
+    recurso = forms.CharField()
+    proposta = forms.CharField()
+    relacionamento = forms.CharField()
+    canal = forms.CharField()
+    segmento = forms.CharField()
+    custo = forms.CharField()
+    receita = forms.CharField()
